f8900d8477e340bcffc5f1aafccf4dc713067c57
jrmart12 <rodolfojose@live.com>
2022-09-01 19:10:29 -0600

    netlify done

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify/functions";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
   integrations: [react(), image()],
 });
