551f4ab233658c20e9e2ba3db0e918734982c532
jrmart12 <rodolfojose@live.com>
2022-10-22 15:12:09 -0600

    about company

--- astro/public/js/index.js
+++ astro/public/js/index.js
@@ -26,3 +26,57 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+function revealText() {
+  var reveals = document.querySelectorAll(".revealText");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealText);
+
+function revealTextInner() {
+  var reveals = document.querySelectorAll(".revealText-inner");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealTextInner);
+
+function revealImage() {
+  var reveals = document.querySelectorAll(".revealImage-inner");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealImage);
