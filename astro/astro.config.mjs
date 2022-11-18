5974dd82b89e4bd1e800d7635a1c39d1873dd075
jrmart12 <rodolfojose@live.com>
2022-11-18 12:42:23 -0600

    added seo and sidemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()],
+  site: "http://newperspectivecayman.com",
+  integrations: [react(), image(), mdx(), sitemap()],
 });
---
+++ astro/src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'New Perspective Cayman';
+export const SITE_DESCRIPTION = 'New Perspective Ltd. Design and Construction detail passion innovation ';
