edde4c8ab9e007ec2d433318310f0f797edfcff6
jrmart12 <rodolfojose@live.com>
2022-12-06 19:36:18 -0600

    change to image

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,9 +3,10 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://newperspectivecayman.com",
-  integrations: [react(), image(), mdx(), sitemap()],
+  integrations: [react(), image(), mdx(), sitemap(), compress()],
 });
